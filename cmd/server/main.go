35655dd5ae7c22391b738b38bda94e42cfcebd4d
silsgah <gahsilas@gmail.com>
2021-11-28 21:58:29 +0000

    Structuring App

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+type App struct {
+}
+
+func (app *App) Run() error {
+	fmt.Println("Setting up App")
+	return nil
+}
 func main() {
 	fmt.Println("GO REST")
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Error starting up our Rest api")
+		fmt.Println(err)
+	}
 }
