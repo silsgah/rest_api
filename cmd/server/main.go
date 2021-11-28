713e22ba90e4ed12e9116e55cce6acf4b3562b32
silsgah <gahsilas@gmail.com>
2021-11-28 22:23:48 +0000

    Added health check end point

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	transportHTTP "github.com/Tutorial/rest_api/internal/transport/http"
+)
 
 type App struct {
 }
 
 func (app *App) Run() error {
 	fmt.Println("Setting up App")
+	handler := transportHTTP.NewHandler()
+	handler.SetupRoutes()
+	if err := http.ListenAndServe(":8080", handler.Router); err != nil {
+		fmt.Println("failed to setup server")
+		return err
+	}
 	return nil
 }
 func main() {
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Tutorial/rest_api
 
 go 1.17
+
+require github.com/gorilla/mux v1.8.0
